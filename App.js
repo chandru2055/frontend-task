792f95d536c81bb6d4a6b6522f96a8d80e97cca6
chandru2055 <65714677+chandru2055@users.noreply.github.com>
2021-03-24 13:33:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import RecommendedVideos from "./RecommendedVideos";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header/>
+      <div className="app_page">
+      <Sidebar/>
+      <RecommendedVideos/>
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
